26aca25c2a2ad618e79f59f169538869d476f73f
phamdinhlam105 <heartsstealer105@gmail.com>
2023-01-02 19:56:47 +0700

    calculator

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,11 +10,47 @@ namespace test
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            System.Console.WriteLine("Hello World, this is test file, give a number");
-            a = Console.Read();
-            Console.WriteLine(a.ToString());
-        
+            int a, b = 0;
+            string n ;
+            //System.Console.WriteLine("Hello World, this is test file, give a number");
+            //string doc;
+            //doc = Console.ReadLine();
+            test.Program.xinchao(); //call method
+            Console.WriteLine("Chon phep tinh ( cong, tru, nhan, chia)");
+
+            n = Console.ReadLine();
+
+            Console.WriteLine("nhap a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}", a);
+            
+            Console.WriteLine("nhap b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("b = {0}", b);
+            switch (n){
+                case "cong": Console.WriteLine("a + b = {0}", Tinhtoan.cong(a, b));
+                    break;
+                case "tru":
+                    Console.WriteLine("a - b= {0}", Tinhtoan.tru(a, b));
+                    break;
+                case "nhan":
+                    Console.WriteLine("a x b = {0}", Tinhtoan.nhan(a, b));
+                    break;
+                case "chia":
+                    Console.WriteLine("a : b = {0}", Tinhtoan.chia(a, b));
+                    break;
+                default:
+                    Console.WriteLine("sai phep tinh roi");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+        //method
+        static void xinchao()
+        {
+            Console.WriteLine("hello, this is a calculator");
+
         }
     }
 }
---
+++ test/test/Tinhtoan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Tinhtoan
+    {
+        public static int cong(int a, int b)
+        {
+            return a + b;
+        }
+        public static int tru(int a, int b)
+        {
+            return a - b;
+        }
+        public static int nhan(int a, int b)
+        {
+            return a * b;
+        }
+        public static float chia(int a, int b)
+        {
+            if(b==0)
+            {
+                Console.WriteLine("ko chia duoc");
+                return 0; }    
+               
+            else
+                return (float)a / b;
+        }
+    }
+}
